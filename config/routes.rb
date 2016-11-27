fd9c8e42951fc84d8696e8fa1bb354de46a32779
Sarah Ing <sarahikeda@gmail.com>
2016-11-27 11:12:02 -0600

    Change root path to user login

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'emails#index'
+  root 'users#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
