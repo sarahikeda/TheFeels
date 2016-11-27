e5ad6f5c394f4328c19588b0c741103ad9c2fc7b
Sarah Ing <sarahikeda@gmail.com>
2016-11-26 20:15:30 -0600

    Add field for partner's email

--- db/migrate/20150905235139_create_partners.rb
+++ db/migrate/20150905235139_create_partners.rb
@@ -2,6 +2,7 @@ class CreatePartners < ActiveRecord::Migration
   def change
     create_table :partners do |t|
       t.string :name
+      t.string :email
       t.integer :user_id
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160920174718) do
 
   create_table "partners", force: :cascade do |t|
     t.string   "name"
+    t.string   "email"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
