c52e32df2f895896d85ef94a862eae20ce604d35
Sarah Ing <sarahikeda@gmail.com>
2016-11-26 22:30:32 -0600

    Re-add sentiment field to emails

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20160920174718) do
     t.datetime "sent_at"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "sentiment"
+    t.float    "score"
   end
 
   create_table "partners", force: :cascade do |t|
