b22e09ae7a55119e14e20e392e3162260ac1398e
Sarah Ing <sarahikeda@gmail.com>
2016-12-10 11:05:23 -0600

    Remove username field

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 20161209211236) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
