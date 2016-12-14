8745f7fd72d91057e5d5162301c9cad932c43725
Sarah Ing <sarahikeda@gmail.com>
2016-12-14 16:59:00 -0600

    Change setting of current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
   def current_user
-    @user ||= User.find(session[:user_id]) if session[:user_id]
+    @user = User.find(session[:user_id]) if session[:user_id]
   end
 
 end
