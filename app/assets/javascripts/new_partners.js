8a5296a4ebb0ca5e45dfeefad8788325aae7fb12
Sarah Ing <sarahikeda@gmail.com>
2016-12-19 15:55:54 -0600

    Add animation for slider

--- app/assets/javascripts/new_partners.js
+++ app/assets/javascripts/new_partners.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $('.add-partner').click(function(){
     $('.new-partner').show();
     $('.add-partner').hide();
+  });
 
-  })
+  $(".sentiment-spacing").rangeslider({
+    polyfill: false
+  });
 });
