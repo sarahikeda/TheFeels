1b37ad59d769801bf72e7c1c9db904e216f354ad
Sarah Ing <sarahikeda@gmail.com>
2016-12-21 16:12:48 -0600

    Redirect to use profile rather than its edit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to edit_user_path(user)
+    redirect_to user_path(user)
   end
 
   def destroy
