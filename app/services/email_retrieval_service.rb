daf46a63833caea59135a40824512d6e20559996
Sarah Ing <sarahikeda@gmail.com>
2016-12-29 01:03:38 -0800

    Add more regex for clean up of text

--- app/services/email_retrieval_service.rb
+++ app/services/email_retrieval_service.rb
@@ -5,7 +5,7 @@ class EmailRetrievalService
   end
 
   def retrieve_emails(partner)
-    @emails = @gmail.inbox.emails(from: partner.email).first(10)
+    @emails = @gmail.inbox.emails(from: partner.email).last(10)
     save_emails(partner)
     @email_count = @emails.count
   end
@@ -19,8 +19,22 @@ class EmailRetrievalService
 
   def clean_email_text(email)
     text = ActionView::Base.full_sanitizer.sanitize(email.body.decoded)
-    text.gsub(/^.+text\/plain; charset=UTF-8/,'').gsub(/\\n/,'')
-    text = RbLibText.tokens(text).join(" ")
+    cleaned_headers = remove_headings(text)
+    cleaned_spaces = remove_spaces(cleaned_headers)
+    cleaned_text = remove_numbers(cleaned_spaces)
+    return RbLibText.tokens(cleaned_text).join(" ")
+  end
+
+  def remove_headings(text)
+    text.gsub(/.*Content-Transfer-Encoding/,'').gsub(/.*Content-Type.*[uUtTfF]-8/,'')
+  end
+
+  def remove_spaces(text)
+    text.gsub(/\\n/,'')
+  end
+
+  def remove_numbers(text)
+    text.gsub(/--.?\d*/,'')
   end
 
 end
